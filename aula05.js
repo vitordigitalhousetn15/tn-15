980cae34fb1bf875a6f5c241a88cc139ed5c1634
Vitor Costa <vjose@digitalhouse.com>
2020-12-03 20:52:17 -0300

    primeiro commit

--- aula05.js
+++ aula05.js
@@ -3,3 +3,12 @@ function mostrarNome() {
 }
 
 console.log("vitor")
+
+
+// git commit
+// $ git config --global user.name "Fulano de Tal"
+// $ git config --global user.email "fulanodetal@exemplo.br"
+
+// git branch -M main
+// git remote add origin https://github.com/vitordigitalhousetn15/tn-15.git
+// git push -u origin main
