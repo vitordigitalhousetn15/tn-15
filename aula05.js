4e0aa48964045e256e051ac75ec51ef5921e5626
Vitor Costa <vjose@digitalhouse.com>
2020-12-03 21:33:56 -0300

    adicionando getProdutos]

--- aula05.js
+++ aula05.js
@@ -4,11 +4,6 @@ function mostrarNome() {
 
 console.log("vitor")
 
-
-// git commit
-// $ git config --global user.name "Fulano de Tal"
-// $ git config --global user.email "fulanodetal@exemplo.br"
-
-// git branch -M main
-// git remote add origin https://github.com/vitordigitalhousetn15/tn-15.git
-// git push -u origin main
+function getProdutos() {
+    return "geladeira"
+}
